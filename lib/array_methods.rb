d2271f2b8e881bff3ae94780439e32d413099766
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-29 00:19:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,7 +18,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  result = 0
+  result = array[0]
   array.length.times do |index|
     if array[index] < result
       result = array[index]
